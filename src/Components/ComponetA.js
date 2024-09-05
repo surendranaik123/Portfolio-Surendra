67c5aac35d09813c0272c3882a8c4a588fd77d60
surendranaik123 <131369740+surendranaik123@users.noreply.github.com>
2024-09-05 16:33:09 +0530

    changes

---
+++ src/Components/ComponetA.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ComponetA() {
+  return (
+    <div>ComponetA</div>
+  )
+}
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -15,7 +15,8 @@ function Projects() {
 
   return (
     <div className='home'>
-      <Header />
+      <Header /> 
+      <div style={{display:"flex"}}>
       <div class="card" style={{width:"18rem"}}>
   <img src={img} class="card-img-top" alt="..."/>
   <div class="card-body">
@@ -24,6 +25,30 @@ function Projects() {
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+
+<div class="card" style={{width:"18rem"}}>
+  <img src={img} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="">Card title</h5>
+    <p class="">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>    <div class="card" style={{width:"18rem"}}>
+  <img src={img} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="">Card title</h5>
+    <p class="">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>    <div class="card" style={{width:"18rem"}}>
+  <img src={img} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="">Card title</h5>
+    <p class="">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
     </div>
   );
 }
